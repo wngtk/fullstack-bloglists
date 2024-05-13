2c13f13b0b2c89b172375a7c72b698e0492e857f
Wang Tiaoke <fnmain.rs@gmail.com>
2024-05-13 14:18:15 +0800

    4.18: bloglist expansion, step6

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,6 +24,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(function errorHandler(error, req, res, next) {
   console.log(error.message);
   if (error.name === 'CastError') {
